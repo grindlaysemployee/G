cd492b9eb51efe69cba7f8783fb4d7802eb92343
grindlaysemployee <operations@grindlaysindia.com>
2025-07-28 11:09:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+async function login() {
+  const empid = document.getElementById("empid").value;
+  const password = document.getElementById("password").value;
+
+  const apiUrl = `https://script.google.com/macros/s/your_script_id/exec?empid=${empid}&password=${password}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.success) {
+      localStorage.setItem("employee", JSON.stringify(data));
+      window.location.href = "dashboard.html";
+    } else {
+      document.getElementById("error").innerText = data.message;
+    }
+  } catch (err) {
+    document.getElementById("error").innerText = "API error.";
+  }
+}
